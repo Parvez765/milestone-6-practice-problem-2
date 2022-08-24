92fe96c9c51162ee8e9617d268f606564addd27b
Pervez <pervezhossain1998@gmail.com>
2022-08-25 01:31:01 +0600

    javascript question addedd

---
+++ question.js
@@ -0,0 +1,19 @@
+// What’s the difference between map, foreach, filter?
+
+/* The forEach() method does not create a new array based on the given array. The map() method creates an entirely new array. The forEach() method returns “undefined“. The map() method returns the newly created array according to the provided callback function.  The filter() method creates a new array filled with elements that pass a test provided by a function. The filter() method does not execute the function for empty elements. The filter() method does not change the original array. */
+
+// What’s the difference between filter and find?
+
+/* The difference between filter and find is that - filer return a new array of element which passes the test consition provided by a function where find return only the first element of an array which passes the condition */
+
+// What is the difference between for..of and for..in?
+
+/* for..in iterates over all enumerable property keys of an object. for..of iterates over the values of an iterable object */
+
+// How do you empty an array?
+
+/* Splice the whole array. arr.splice(0, arr.length) This will remove all elements from the array and will actually clean the original array. */
+
+// Difference between class and object.
+
+/* Object is an instance of a class. Class is a blueprint or template from which objects are created. */
